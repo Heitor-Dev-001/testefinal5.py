94051f783d8e16317c3c6343d7fbc9da4be55965
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:26:32 -0300

    Add files via upload

---
+++ testefinal005.py
@@ -0,0 +1,8 @@
+#Considerando as variáveis n='José' e i=25, 
+# qual das opções abaixo é a única válida como uma string 
+# formatada que funcionaria em Python?
+
+n = 'jose'
+i = '25'
+print('Você se chama {} e tem {} anos de idade'.format(n, i))
+
